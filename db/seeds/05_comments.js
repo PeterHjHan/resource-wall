fdd524d2d7b85b8df8dc0c6a3a8244c171fb9853
= <peterhjhan@gmail.com>
2018-10-18 21:42:38 +0000

    update seed comments and renaming files for seed and migration order

--- db/seeds/05_comments.js
+++ db/seeds/05_comments.js
@@ -1,13 +1,27 @@
-
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('comments').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('comments').insert({
+          id: 1,
+          comment: 'ID 1: this was the best thing ever',
+          user_id: 2,
+          resource_id: 2
+        }),
+        knex('comments').insert({
+          id: 2,
+          comment: 'ID2: This wasn not the best thing ever',
+          user_id: 3,
+          resource_id: 3
+        }),
+        knex('comments').insert({
+          id: 3,
+          comment: 'ID 3: HELLO WORLD',
+          user_id: 1,
+          resource_id: 1
+        })
       ]);
     });
 };
