a9ab9f4861b7b8788ee7d0d7a9ad928ac1e31cf4
= <peterhjhan@gmail.com>
2018-10-18 20:47:32 +0000

    complete seed user

--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -1,13 +1,27 @@
-
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('users').insert({
+          id: 1,
+          username: 'peter',
+          password: 'admin',
+          avatar: 'http://emulation.gametechwiki.com/images/thumb/f/f5/Teknoparrot.png/210px-Teknoparrot.png'
+        }),
+        knex('users').insert({
+          id: 2,
+          username: 'bassem',
+          password: 'admin',
+          avatar: 'http://emulation.gametechwiki.com/images/thumb/f/f5/Teknoparrot.png/210px-Teknoparrot.png'
+        }),
+        knex('users').insert({
+          id: 3,
+          username: 'monica',
+          password: 'admin',
+          avatar: 'http://emulation.gametechwiki.com/images/thumb/f/f5/Teknoparrot.png/210px-Teknoparrot.png'
+        })
       ]);
     });
 };
