60d477a2ba7c3e9b0d6c69ab11ecd7cbac358f80
= <peterhjhan@gmail.com>
2018-10-18 21:07:13 +0000

    create ratings raw data

--- db/seeds/04_ratings.js
+++ db/seeds/04_ratings.js
@@ -1,13 +1,27 @@
-
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('ratings').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('ratings').insert({
+          id: 1,
+          rating: 1,
+          user_id: 1, 
+          resource_id: 2
+        }),
+        knex('ratings').insert({
+          id: 2,
+          rating: 2,
+          user_id: 2, 
+          resource_id: 3
+        }),
+        knex('ratings').insert({
+          id: 3,
+          rating: 3,
+          user_id: 3, 
+          resource_id: 1
+        })
       ]);
     });
 };
