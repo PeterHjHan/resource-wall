07a14aacf6eb5fac35953b16cfcece797d94149e
= <peterhjhan@gmail.com>
2018-10-18 21:30:11 +0000

    commit before file change name of topics

--- db/migrations/20181018210727_topics.js
+++ db/migrations/20181018210727_topics.js
@@ -1,11 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('tables', (table) =>  {
+  return knex.schema.createTable('topics', (table) =>  {
     table.increments('id').primary();
     table.string('name').notNull();
   })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('tables');
+  return knex.schema.dropTable('topics');
 };
