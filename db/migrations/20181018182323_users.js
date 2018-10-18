2f434dff5af31490aeba573c9f3f66cfff771507
= <peterhjhan@gmail.com>
2018-10-18 18:53:25 +0000

    add schema users

--- db/migrations/20181018182323_users.js
+++ db/migrations/20181018182323_users.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id');
+    table.string('username');
+    table.string('password');
+    table.string('avatar');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users');
 };
