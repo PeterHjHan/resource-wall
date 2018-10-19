26462ebbc23300fa54d93bd98352156e307e88c9
= <peterhjhan@gmail.com>
2018-10-19 22:36:26 +0000

    join knex resources and ratings

--- routes/resources.js
+++ routes/resources.js
@@ -9,6 +9,7 @@ module.exports = (knex) => {
     knex
       .from("resources")
       .join("likes", "resources.id", "=", "likes.resource_id")
+      .join("ratings", "resources.id", "=", "ratings.resource_id")
       .select("*")
       .then((results) => {
         res.json(results);
