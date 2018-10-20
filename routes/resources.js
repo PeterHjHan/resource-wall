5fb02a487b6f3f12a0638d4fe8425fb1d85ad80b
= <peterhjhan@gmail.com>
2018-10-20 02:36:02 +0000

    comment join in resources due to bug

--- routes/resources.js
+++ routes/resources.js
@@ -1,20 +1,21 @@
 "use strict";
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
-    knex
-      .from("resources")
-      .join("likes", "resources.id", "=", "likes.resource_id")
-      .join("ratings", "resources.id", "=", "ratings.resource_id")
+    knex("resources")
+
       .join("topics", "resources.topic_id", "=", "topics.id")
+      //.join("likes", "likes.resource_id", "=", "resources.id")
+      // .join("ratings", "resources.id", "=", "ratings.resource_id")
+
       .select("*")
       .then((results) => {
         res.json(results);
-    });
+      });
   });
 
   return router;
