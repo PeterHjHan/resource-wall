66a49a01f0143bc5ba8f526178cfeec73c2e4a0d
= <peterhjhan@gmail.com>
2018-10-20 21:42:51 +0000

    use the this.id causes issue

--- routes/resources.js
+++ routes/resources.js
@@ -11,7 +11,7 @@ module.exports = (knex) => {
       .join("topics", "resources.topic_id", "=", "topics.id")
       .leftJoin("likes", "likes.resource_id", "=", "resources.id")
       .leftJoin("ratings", "resources.id", "=", "ratings.resource_id")
-      .select('resources.id','resources.title', 'topics.topic')
+      .select('resources.id', 'resources.title', 'topics.topic', 'likes.resource_id', 'ratings.resource_id', 'resources.topic_id')
       .then((results) => {
         res.json(results);
       });
