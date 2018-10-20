06877b469908b94ac7f26868a2bace3f112ffd40
= <peterhjhan@gmail.com>
2018-10-20 21:39:19 +0000

    selecting topics.id causes problem

--- routes/resources.js
+++ routes/resources.js
@@ -8,11 +8,10 @@ module.exports = (knex) => {
   router.get("/", (req, res) => {
     knex("resources")
 
-      // .join("topics", "resources.topic_id", "=", "topics.id")
-      // .leftJoin("likes", "likes.resource_id", "=", "resources.id")
-      // .leftJoin("ratings", "resources.id", "=", "ratings.resource_id")
-
-      .select("*")
+      .join("topics", "resources.topic_id", "=", "topics.id")
+      .leftJoin("likes", "likes.resource_id", "=", "resources.id")
+      .leftJoin("ratings", "resources.id", "=", "ratings.resource_id")
+      .select('resources.id','resources.title', 'topics.topic')
       .then((results) => {
         res.json(results);
       });
--- server.js
+++ server.js
@@ -39,19 +39,20 @@ const topicsRoutes = require('./routes/topics');
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
-app.use(knexLogger(knex));
+// app.use(knexLogger(knex));
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/styles', sass({
-  src: __dirname + '/styles',
-  dest: __dirname + '/public/styles',
-  debug: true,
-  outputStyle: 'expanded'
-}));
+
+// app.use('/styles', sass({
+//   // src: __dirname + '/styles',
+//   // dest: __dirname + '/public/styles',
+//   // debug: true,
+//   // outputStyle: 'expanded'
+// }));
 app.use(express.static('public'));
 
 app.use(cookieSession({
