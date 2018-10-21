03a657a4bf035d56eaba35bf4aee96212e5bec98
= <peterhjhan@gmail.com>
2018-10-21 07:12:54 +0000

    continue fix with dropdown for topics

--- public/scripts/client-functions.js
+++ public/scripts/client-functions.js
@@ -80,9 +80,13 @@ function renderTopicsInNavBar() {
     url: "/api/topics"
   }).then((topics) => {
     for (item of topics) {
-      $('<a>').attr(`href`,`/topics/${item.topic}`).addClass('dropdown-item').text(item.topic)
-        .appendTo('.dropdown-menu');
+      const $li = $('<li>')
+      const $topic = $('<a>').attr(`href`,`/topics/${item.topic}`).addClass('dropdown-item').text(item.topic)
+
+      $li.append($topic)
+         .appendTo('.dropdown-menu1');
     }
+
   });
 }
 
