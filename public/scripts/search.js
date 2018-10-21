fce832f0afaa3496148be23b57b1c629b721fe2e
Bassem Kaddour <bassemkaddour@gmail.com>
2018-10-21 05:13:02 +0000

    Fixed search function to match new select names

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,13 +1,12 @@
 $(() => {
   var urlParams = new URLSearchParams(window.location.search);
   var urlParamsString = urlParams.get('query');
-  
+
   $.ajax({
     method: "GET",
     url: "/api/search",
     data: urlParamsString
   }).then((resources) => {
-
   for(resource of resources) {
     createResourceElement(resource).appendTo($("body"));
   }
